e5fb379ad0a8652fc2e58e9cdc0464daa49317c0
Malt <1072829851@qq.com>
2018-02-08 17:51:10 +0800

    Palindrome Linked List

--- src/234.cpp
+++ src/234.cpp
@@ -5,7 +5,7 @@
 // 由于采用了栈，则空间复杂度不为O(1)。
 
 // 对于采用O(1)复杂度的方法，首先通过快慢指针找到中间结点，然后对链表后半段进行反
-// 转，然后将反转的后半段与前半段比较。
+// 转，然后将反转的后半段与前半段比较。见注释。
 
 //Given a singly linked list, determine if it is a palindrome.
 /**
@@ -50,3 +50,54 @@ public:
 		return result;
     }
 };
+
+//方法二
+/*
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+		if (!head || !head->next)
+			return true;
+		bool result = true;
+		ListNode *slow = head;
+		ListNode *fast = head;
+		while (fast->next && fast->next->next) //快慢指针找到中点
+		{
+			slow = slow->next;
+			fast = fast->next->next;
+		}
+		ListNode *middle = slow->next;
+		
+		ListNode *prev_node = nullptr;   //反转后半部分链表
+		ListNode *cur_node = middle;
+		ListNode *next_node = middle->next;
+		while (next_node)
+		{
+			cur_node->next = prev_node;
+			prev_node = cur_node;
+			cur_node = next_node;
+			next_node = next_node->next;
+			if (next_node == nullptr)
+				cur_node->next = prev_node;
+		//当next为空，表示curnode为尾结点，设置curnode的next指针，这一点非常
+		重要。因为curnode原来的next指向空，现在需要将其指向prev。
+		}
+		ListNode *behind_half = cur_node;
+		
+		ListNode *p = head;
+		ListNode *q = behind_half;
+		while (p && q)   //遍历前半部和后半部结点。
+		{
+			if (p->val != q->val)
+			{
+				result = false;
+				break;
+			}
+			p = p->next;
+			q = q->next;
+		}
+		
+		return result;
+    }
+};
+*/
