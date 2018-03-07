3a0b1e099a76f596f2fd5dc6b1b4d84b2d02b9b1
Malt <1072829851@qq.com>
2018-03-07 10:06:34 +0800

    Flood Fill（采用dfs和bfs）

--- src/733.cpp
+++ src/733.cpp
@@ -3,6 +3,13 @@
 // 主要是需要考虑对边界条件的判断。
 
 //dfs
+// 遍历到当前节点，将当前节点填充并标记当前节点已访问，分别递归访问当前节点上下左右四个方向满足条件的节点。
+//需要维护一个visit，用于标记当前位置的节点是否被访问。
+// 满足条件即：
+// 1）该处的坐标满足边界要求；
+// 2）未被访问；
+// 3）该处的颜色与初始节点相同。
+
 class Solution {
 public:
 	bool issame(vector<vector<int>> &image, int sr, int sc, int original)
@@ -44,6 +51,12 @@ private:
 };
 
 //bfs
+// 遍历当前节点，设置当前节点为已访问并将当前节点入队。只要队不为空，队头出队，并判断队头上下左右四个方向上的节点是否满足访问。
+// 要求，如果满足则将该处节点入队，并标记为已访问。
+// 满足访问要求，即：
+// 1）坐标位置没有越界；
+// 2）未被访问；
+// 3）该处颜色与原始节点颜色相同。
 class Solution {
 public:
 	bool issame(vector<vector<int>> &image, int sr, int sc, int original)
